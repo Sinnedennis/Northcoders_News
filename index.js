22ffba6799e063f4eb7fd668116ab59ea7aa775d
Dennis Foster <contact@DennisFoster.co.uk>
2017-12-14 11:51:56 +0000

    Adding default port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./server');
-const PORT = require('./config').PORT[process.env.NODE_ENV];
+const PORT = require('./config').PORT[process.env.NODE_ENV] || 3000;
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`); // eslint-disable-line
